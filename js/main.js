c60cdfb51aebafa327929eeaa29c5af3a09b2055
Gaston Montecino <montecino.gd@gmail.com>
2024-07-27 23:11:15 -0300

    Con calculo del tipo de respuestagit add .!

--- js/main.js
+++ js/main.js
@@ -19,15 +19,31 @@ function calcularRapido () {
     factor = parseFloat(document.getElementById("factor-rapido").value)
     let relacion= ratio(copiasBcr, copiasAbl)
     let ratioCorregido = correccion (relacion, factor)
-        
+    
+    let filtradoAbl = respuestas.filter(rta => rta.ablMin <= copiasAbl)
+    console.log(filtradoAbl)
+
     if ( isNaN(factor) || factor === 0){
         let resultadoTexto = document.getElementById("resultado-rapido")
         resultadoTexto.innerText = "Relacion BCR::ABL/ABL % = "+relacion.toFixed(4)
         let notaResultadoTexto = document.getElementById("no-posee-factor")
         notaResultadoTexto.innerText = "Usted no ingreso un factor de correción"
+
+        let busqueda = filtradoAbl.find(rta => rta.limiteSup >= relacion)
+        console.log(busqueda.molecular)
+
+        let respuestaMolecularTexto = document.getElementById("respuesta-molecular")
+        respuestaMolecularTexto.innerText = "Respuesta molecular "+ busqueda.molecular
+
     } else {
         let resultadoTexto = document.getElementById("resultado-rapido")
         resultadoTexto.innerText = "Relación BCR::ABL/ABL % corregido = "+ratioCorregido.toFixed(4)
+        let busqueda = filtradoAbl.find(rta => rta.limiteSup >= ratioCorregido)
+        console.log(busqueda.molecular)
+
+        let respuestaMolecularTexto = document.getElementById("respuesta-molecular")
+        respuestaMolecularTexto.innerText = "Respuesta molecular "+ busqueda.molecular
+
     }
     
     }
@@ -64,25 +80,33 @@ function cargarPaciente(){
     let ratioCorregido = correccion (relacion, cargaFactor)
     console.log(ratioCorregido)
 
-
+    let filtradoAbl = respuestas.filter(rta => rta.ablMin <= cargaAbl)
+    console.log(filtradoAbl)
+    
     let pacienteIngresado = document.getElementById("paciente-container")
 
     let nuevoPaciente = document.createElement("p");
         if ( isNaN(cargaFactor) || cargaFactor === 0){
-            nuevoPaciente.innerHTML =`<p> Nombre: ${paciente.nombre} </p> 
+            let busqueda = filtradoAbl.find(rta => rta.limiteSup >= relacion)
+            console.log(busqueda.molecular)
+            nuevoPaciente.innerHTML =`<p id="prueba"> Nombre: ${paciente.nombre} </p> 
                             <p>Numero de muestra: ${paciente.numero}</p> 
                             <p>Fecha: ${paciente.fecha}</p>
                             <p>Tipo de muestra: ${paciente.muestra}</p>
                             <p>Copias de ABL: ${cargaAbl} copias</p>
-                            <p> BCR::ABL: ${relacion.toFixed(4)}% </p>` //Mucho cuidado con las comillas aca!!!
+                            <p> BCR::ABL: ${relacion.toFixed(4)}% </p>
+                            <p> Respuesta molecular: ${busqueda.molecular}</p>` //Mucho cuidado con las comillas aca!!!
         document.body.appendChild(nuevoPaciente)
         } else {
+            let busqueda = filtradoAbl.find(rta => rta.limiteSup >= ratioCorregido)
+            console.log(busqueda.molecular)
             nuevoPaciente.innerHTML =`<p> Nombre: ${paciente.nombre} </p> 
                             <p>Numero de muestra: ${paciente.numero}</p> 
                             <p>Fecha: ${paciente.fecha}</p>
                             <p>Tipo de muestra: ${paciente.muestra}</p>
                             <p>Copias de ABL: ${cargaAbl} copias</p>
-                            <p> BCR::ABL corregido: ${ratioCorregido.toFixed(4)}% </p>` //Mucho cuidado con las comillas aca de nuevo!!!
+                            <p> BCR::ABL corregido: ${ratioCorregido.toFixed(4)}% </p>
+                            <p> Respuesta molecular: ${busqueda.molecular}</p>` //Mucho cuidado con las comillas aca de nuevo!!!
         document.body.appendChild(nuevoPaciente)
         }
 
@@ -90,4 +114,61 @@ function cargarPaciente(){
 
 let botonCargaCalculo = document.getElementById("btn-calcular")
 botonCargaCalculo.addEventListener("click", cargarPaciente)
+
+
+
+const rta1 = {
+    molecular: "RM 5.0",
+    reduccionLogaritmica: ">= 5.0 log",
+    limiteInf: 0,
+    limiteSup: 0.001,
+    abl: 100000
+}
+
+const rta2 = {
+    molecular: "RM 4.5",
+    reduccionLogaritmica: ">= 4.5 log",
+    limiteInf: 0,
+    limiteSup: 0.0032,
+    ablMin: 32000
+}
+
+const rta3 = {
+    molecular: "RM 4.0",
+    reduccionLogaritmica: ">= 4.0 log",
+    limiteInf: 0,
+    limiteSup: 0.01,
+    ablMin: 10000
+}
+
+const rta4 = {
+    molecular: "RM Mayor",
+    reduccionLogaritmica: ">= 3.0 log",
+    limiteInf: 0.01,
+    limiteSup: 0.1,
+    ablMin: 0
+}
+const rta5 = {
+    molecular: "RM Menor",
+    reduccionLogaritmica: ">= 2.0 log",
+    limiteInf: 0.1,
+    limiteSup: 1,
+    ablMin: 0
+}
+const rta6 = {
+    molecular: "RM Minima",
+    reduccionLogaritmica: ">= 1.0 log",
+    limiteInf: 1,
+    limiteSup: 10,
+    ablMin: 0
+}
+const rta7 = {
+    molecular: "RM Nula",
+    reduccionLogaritmica: "< 1.0 log",
+    limiteInf: 10,
+    limiteSup: Infinity,
+    ablMin: 0
+}
+
+const respuestas = [rta1, rta2, rta3, rta4, rta5, rta6, rta7]
 
